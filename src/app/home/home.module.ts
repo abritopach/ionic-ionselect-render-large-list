3f691f73936a69280c995bb98d2006576d86f49f
abritopach <adrbrpa1988@gmail.com>
2020-05-31 22:35:56 +0200

    Added angular cdk

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,9 +1,14 @@
+// Angular
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
-import { HomePage } from './home.page';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
+// Ionic
+import { IonicModule } from '@ionic/angular';
+
+// Project
+import { HomePage } from './home.page';
 import { HomePageRoutingModule } from './home-routing.module';
 
 
@@ -12,7 +17,8 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    ScrollingModule
   ],
   declarations: [HomePage]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,60 @@
-import { Component } from '@angular/core';
+// Angular
+import { Component, OnInit } from '@angular/core';
+
+// Ionic
+import { AlertController } from '@ionic/angular';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  constructor() {}
+    items: any;
+
+    constructor(private alertController: AlertController) {}
+
+    ngOnInit() {
+        this.items = Array.from({length: 100000}).map((_, i) => `Item ${i}`);
+    }
+
+    async presentAlert() {
+
+        let itemsList = ``;
+
+        this.items.map((item) => {
+            itemsList +=
+            `<ion-item>
+                    <ion-label>${item}</ion-label>
+            </ion-item>
+            `;
+        });
+
+        const message = `<ion-list>${itemsList}</ion-list>`;
+
+        const alert = await this.alertController.create({
+            header: 'List virtual scrolling',
+            message
+            ,
+            buttons: [
+                {
+                    text: 'Cancel',
+                    role: 'cancel',
+                    cssClass: 'secondary',
+                    handler: (blah) => {
+                        console.log('Confirm Cancel: blah');
+                    }
+                },
+                {
+                    text: 'Ok',
+                    handler: () => {
+                        console.log('Confirm Okay');
+                    }
+                }
+            ]
+        });
+        await alert.present();
+    }
 
 }
